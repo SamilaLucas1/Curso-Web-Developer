f7cd8141f646db88f5e7556e0c0c92a834d3bfa3
Samila Lucas <samilalucas2004@gmail.com>
2022-12-01 16:08:17 -0300

    add mod 7 and add 8

--- JavaSript/Class-07-array-metthods/script.js
+++ JavaSript/Class-07-array-metthods/script.js
@@ -41,14 +41,14 @@
 
 // console.log(real.filter((value) => value < 40))
 
-const averageStudants = [
-    {id: 1, name: 'João', average: 4, birth: 1990},
-    {id: 2, name: 'Thiago', average: 6, birth: 2002},
-    {id: 3, name: 'Jonas', average: 5, birth: 1998},
-    {id: 4, name: 'Rosa', average: 9, birth: 2004},
-    {id: 5, name: 'Amanda', average: 8, birth: 1992},
-    {id: 6, name: 'Pedro', average: 7, birth: 1993},
-]
+// const averageStudants = [
+//     {id: 1, name: 'João', average: 4, birth: 1990},
+//     {id: 2, name: 'Thiago', average: 6, birth: 2002},
+//     {id: 3, name: 'Jonas', average: 5, birth: 1998},
+//     {id: 4, name: 'Rosa', average: 9, birth: 2004},
+//     {id: 5, name: 'Amanda', average: 8, birth: 1992},
+//     {id: 6, name: 'Pedro', average: 7, birth: 1993},
+// ]
 
 // console.table(averageStudants.filter((studant) => studant.average >= 7))
 // console.table(averageStudants.filter((studant) => studant.birth >= 2000))
@@ -64,15 +64,31 @@ const averageStudants = [
 // console.log(averageStudants.find((studant) => (yearCurrent - studant.birth) >= 30))
 // console.log(averageStudants.find((studant) => studant.id == 4))
 
-const teams = ['Brasil', 'Alemanha', 'Argentina', 'Portugal']
+// const teams = ['Brasil', 'Alemanha', 'Argentina', 'Portugal']
 // const real = [10, 20, 30, 40, 50,]
-const real = [10, 30, 50, 20, 40]
+// const real = [10, 30, 50, 20, 40]
 
-console.log(teams.concat(real))
+// console.log(teams.concat(real))
 
-console.log(real.sort((a, b) => a - b ));
-console.log(teams.sort((a, b) => {
-    if (a.toLocaleLowerCase > b.toLocaleLowerCase) return 1;
-    if (a.toLocaleLowerCase < b.toLocaleLowerCase) return -1;
-    return 0
-}))
+// console.log(real.sort((a, b) => a - b ));
+// console.log(teams.sort((a, b) => {
+//     if (a.toLocaleLowerCase > b.toLocaleLowerCase) return 1;
+//     if (a.toLocaleLowerCase < b.toLocaleLowerCase) return -1;
+//     return 0
+// }))
+
+// const teams = ['Brasil', 'Alemanha', 'Argentina', 'Portugal']
+// let saldo = 8000
+// const real = [1, 10, 2, 30, 50, 20, 40]
+// console.log(real.reduce((a, b) => a + b)) // a + b, saldo
+
+const averageStudants = [
+    {id: 1, name: 'João', average: 4, birth: 1990},
+    {id: 2, name: 'Thiago', average: 6, birth: 2002},
+    {id: 3, name: 'Jonas', average: 5, birth: 1998},
+    {id: 4, name: 'Rosa', average: 9, birth: 2004},
+    {id: 5, name: 'Amanda', average: 8, birth: 1992},
+    {id: 6, name: 'Pedro', average: 7, birth: 1993},
+]
+
+console.table(averageStudants.sort((a, b) => a.average - b.average))
---
+++ JavaSript/Class-08-dom/script.js
@@ -0,0 +1,35 @@
+// const myTitle = document.getElementById("myTitle") //element
+// const myTitle = document.querySelector("body h1#myTitle") //element
+
+console.log(myTitle)
+console.log(myTitle.textContent)
+
+// myTitle.innerHTML = "Teste" //innerHTML = modifica o HTML
+// myTitle.innerText = "Teste"  //innerText = modifica o Text
+
+myTitle.style.color = "red"
+
+// const myParagraph = document.getElementsByClassName("paragraph") //HTMLCollection
+
+// console.log(myParagraph)
+
+// for(const value of myParagraph) {
+//     console.log(value)
+// }
+
+// const myParagraph = document.querySelectorAll(".paragraph")
+// console.log(myParagraph)
+
+// myParagraph.forEach((value => value.style.color = "green"))
+
+// const myTitle = document.querySelector("#myText")
+// const myButton = document.querySelector("#myButton")
+// const writeText = document.querySelector("#writeText")
+
+// myButton.addEventListener("click", () =>{
+//     writeText.innerHTML = myText.value
+// })
+
+// myButton.addEventListener("Keypress", () =>{
+//     writeText.innerHTML = myText.value
+// })
